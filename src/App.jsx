25740521f4fc4ebf5fac79895b4a9b52c5833c92
Domingo <30532920+ctagayun@users.noreply.github.com>
2024-09-05 11:32:51 -0500

    Save

--- src/App.jsx
+++ src/App.jsx
@@ -40,7 +40,14 @@ const initialList = [
   },
 ];
 
-//reducerHook is best to use if multiple states are dependent on each other.
+
+
+///React Advance State Reducer Hook
+// see: https://www.robinwieruch.de/react-state-usereducer-usestate-usecontext/
+
+//How to useReducer in React: https://www.robinwieruch.de/react-usereducer-hook/
+
+//useReducer is best to use if multiple states are dependent on each other.
 //For stories, boolean is Loading, and error are all related to data fetching. 
 //All three properties/state could be a part of one complex object (example data, isLoading, error)
 //managed by a reducer
@@ -50,16 +57,46 @@ const initialList = [
 //A reducer function always receives a state and an action. 
 //Based on these two arguments, returns a new state.
 
+//A reducer function always receives a state and an action (A). 
+//Based on these two arguments, a reducer always returns a new state:
+//  1. 'action' is always associated with a type (B) and payload (B)
+//  2. If the type matches a condition in the reducer (B), return a new state (C) 
+//    based on incoming state and action.
+
+
+//In the following example: 
+//The listReducer function covers "one type" and then returns the 
+//payload of the incoming action without using the current state to 
+//compute the new state. The new state is therefore simply the payload.
+
+//Define a reducer function. It always receives state and action
+//   1.'action' is always associated with a type. In this example
+//     there are 2 types: 'ADD_ITEM' and 'DELETE_ITEM' 
+//     each type returns appropriate action.payload.
+//
+//   2. If the type matches a condition  in this case 'ADD_ITEM' 
+//      and 'DELETE_ITEM' the reducer return a new state based on 
+//       incoming state and action.
+//   3. Based on these two arguments the reducer always returns 
+//     a new state
+
+
 const listReducer = (state, action) =>{
-   switch (action.type) {
+   switch (action.type) { //(A)
      //based on the action type implement the business logic
      case 'ADD_ITEM':
        // console.log (`Adding Record. ${action.title} ${action.objectID}`)
         return state.concat({ title: action.title, objectID: action.objectID }); //return a new state
-     case 'DELETE_ITEM':
+    
+        case 'DELETE_ITEM':
         console.log (`Delete  Action Type is = ${action.type}`)
-        console.log (`Deleting Record = ${action.type}`)
-        return state.filter((item) => item.objectID !== item.objectID); //return a new state
+        console.log (`Item to be deleted by the reducer is ${action.payload.objectID}`)
+
+        //Return a new state but filter the state first. In this case objectID. 
+        //Filter generates a new array from the 'state' an call it "story"
+        return state.filter(   //return a new state (C)
+           (story) => action.payload.objectID !== story.objectID  
+          );
 
      default:
         console.log(`Unhandled TYPE: ${action.type}`);
@@ -67,6 +104,34 @@ const listReducer = (state, action) =>{
    }
 
 };
+
+ //Create a custom hook called "useStorageState". We will use two hooks 
+  //to create it:
+  //    1. useState
+  //    2. useEffect 
+
+  //The purpose of this custom hook is to save and fetch from the localtorage
+  //the values that were inputted in the search box.
+  //The actual return value of our custom hook will be displayed in the 
+  //search box.
+
+  const useStorageState = (searchKeyParam, deafaultStateParam) => {
+    const [theState, stateSetter] = React.useState(
+       localStorage.getItem(searchKeyParam) || deafaultStateParam //provides an initial value to the hook.
+    );
+
+    //https://react.dev/reference/react/useEffect#useeffect
+    //Since the key comes from outside, the custom hook assumes that it could change,
+    //so it needs to be included in the dependency array of the useEffect hook as well.
+    React.useEffect(() => {
+        localStorage.setItem(searchKeyParam, theState);
+       },
+       [theState, stateSetter] );
+
+    //Custom hooks return values are returned as an array
+    return [theState, stateSetter]; 
+
+ } //EOF create custom hook
 
 
 //Declaration of App component
@@ -81,15 +146,21 @@ function App() {
   let defaultState = 'React'
 
   
+  //now call our custom hook useLocalStorage to initialize our state 
+  //called "searchTerm". The actual return value of our custom hook is:
+  //return [theState, stateSetter]. But we can rename it. In this case
+  //searchTerm, setSearchTerm respectively
+  const [searchTerm, setSearchTerm] = useStorageState(searchKey, defaultState)
+  
   //This hook returns as an array :
   //     1. a state in this case "listData"
   //     2. a dispatch function in this case "dispatchListData" 
   //which we conveniently access again via array destructuring. 
   // The parameters in this case are:
   //     "listReducer" is the reducer function
-  //     "initialList" is the list which is used to initialize "listData" first time in
+  //     "initialList" is the list which is used to initialize "updatedList" first time in
   //                
-  const [listData, dispatchListData] = React.useReducer(listReducer, initialList );
+  const [updatedList, dispatchListData] = React.useReducer(listReducer, initialList );
 
   //Create a state for the field that will be used to input the new item
   const [title, setTitle] = React.useState('');
@@ -102,33 +173,36 @@ function App() {
 
   const handleAdd = () => {
     console.log(`Item being Added: ${title}`);
-
     //After the below  code is executed "listData" state will contain the added record
     dispatchListData({type: 'ADD_ITEM', title, objectID: uuidv4()}); 
     setTitle('');  //reset the input box to null
   };
 
-  //Function to delete a a record from the initialList list
-  // const handleDeleteRecord = (item) => {
-  //   console.log(`Item being deleted =  ${item.objectID} ${item.author}`);
-  //   const newList = updatedList.filter(
-  //     (story) => item.objectID !== story.objectID
-  //   );
-  //   updateInitialList(newList);
-  // };
-
+ 
   const handleDeleteRecord = (item) => {
     console.log(`Item being deleted =  ${item.objectID} ${item.title}`);
-    dispatchListData({type: 'DELETE_ITEM', title, objectID: uuidv4()}); 
+    dispatchListData({type: 'DELETE_ITEM', payload: item,}); 
   }
 
+  const handleSearch = (event) => {
+    setSearchTerm(event.target.value); //update state hook variable in this case "searchTerm"
+  }
  
+  const searchedList = updatedList.filter((story) =>
+    story.title.toLowerCase().includes(searchTerm.toLowerCase())
+  );
+
   return (
     <div>
        <h1> 
           {welcome.greeting} {welcome.title}
       </h1>
     
+      {/* searchTerm is the return value from useStorageState custom hook. */}
+      <Search id="search" value={searchTerm}  isFocused  onInputChange={handleSearch} >
+         <strong>Search:</strong>
+      </Search>
+
        <hr/>
      <AddItem
         name={title}
@@ -138,7 +212,7 @@ function App() {
 
        {/*We have made the input field "title" a controlled element, because 
          it receives its internal value from React's state now. */}
-       <RenderListUsingArrowFunction list={listData} 
+       <RenderListUsingArrowFunction list={searchedList} 
                                      onRemoveItem={handleDeleteRecord} />
         
     </div>
